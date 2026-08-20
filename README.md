a45fd2b634d48383907e56a918b2518f6a197d5f
Ярослав Селютин <selutin@rasa.pro>
2026-08-20 15:15:57 +1000

    feat: init project

---
+++ README.md
@@ -0,0 +1,8 @@
+# myapp
+
+Wails v3 + React + TypeScript.
+
+```
+wails3 dev
+wails3 build
+```
---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"embed"
+	"log"
+
+	"github.com/wailsapp/wails/v3/pkg/application"
+)
+
+//go:embed all:frontend/dist
+var assets embed.FS
+
+func main() {
+	app := application.New(application.Options{
+		Name:        "myapp",
+		Description: "myapp",
+		Assets: application.AssetOptions{
+			Handler: application.AssetFileServerFS(assets),
+		},
+		Mac: application.MacOptions{
+			ApplicationShouldTerminateAfterLastWindowClosed: true,
+		},
+	})
+
+	app.Window.NewWithOptions(application.WebviewWindowOptions{
+		Title:  "myapp",
+		Width:  1024,
+		Height: 768,
+		Mac: application.MacWindow{
+			InvisibleTitleBarHeight: 50,
+			Backdrop:                application.MacBackdropTranslucent,
+			TitleBar:                application.MacTitleBarHiddenInset,
+		},
+		URL: "/",
+	})
+
+	if err := app.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
